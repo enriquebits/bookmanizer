ed5a70a9514b3e053f155e9a02534fb9c5317319
peguerosdc <dianasaurio@dianasaurio-Inspiron-1525.(none)>
2014-06-15 05:44:32 -0500

    shares

--- pegasus/public/javascript/search_results.js
+++ pegasus/public/javascript/search_results.js
@@ -10,6 +10,7 @@ function loadMore () {
 	if( !finished ) {
 		$.get( "/dashboard/search_ajax?tags="+$("#tags").val()+"&page="+page, function( data ) {
 			var urls = data.urls
+			var ides = data.ides
 			var arrayLength = urls.length;
 			if(arrayLength > 16) {
 				$("#loadMore").attr("disabled", "disabled");				
@@ -21,7 +22,7 @@ function loadMore () {
 			for (var i = 0; i < arrayLength; i++) {
 			    $.get( "http://api.embed.ly/1/oembed?key=93e6adfaf56b44aba78d8e5384bda3b1&url="+urls[i], function( data ) {
 					var img = typeof data.thumbnail_url === "undefined" ? "/img/link_placeholder.jpg" : data.thumbnail_url;
-			    	var res = '<div class="col-md-3"><div class="panel panel-default results-panel"><div class="panel-heading"><h3 class="panel-title">'+data.title+'</h3></div><div class="panel-body" style="background-image: url('+img+');" ><div class="panel-body-footer">'+data.description.substring(0, 66)+'...</div></div><div class="panel-footer">Panel footer</div></div></div>'
+			    	var res = '<div class="col-md-3"><div class="panel panel-default results-panel"><div class="panel-heading"><h3 class="panel-title">'+data.title+'</h3></div><div class="panel-body" style="background-image: url('+img+');" ><div class="panel-body-footer">'+data.description.substring(0, 66)+'...</div></div><div class="panel-footer">Panel footer</div><form id="showPage" method="post" action="/link?lid='+ides[i]+'"></form></div></div>'
 			    	$("#links").append(res);
 			    });
 			}
