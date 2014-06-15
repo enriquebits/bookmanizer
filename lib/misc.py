bf06dbd151b293a73e86f1cced5ee64e4ea866bf
enriquebits <enrique_empo@hotmail.com>
2014-06-15 08:56:44 -0500

    Styling

---
+++ lib/misc.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+""" Misc tools library for Pegasus """
+import tg
+import logging
+from pegasus import model
+log = logging.getLogger(__name__)
+    
+def getUser(request):
+    """ 
+    Return current local user
+    @param session request
+    @return user or None
+    """
+    if request.identity:
+        identity = request.environ.get('repoze.who.identity')
+        #log.debug("identity: %s", identity['repoze.who.userid'])
+        user = model.User.by_user_name(identity['repoze.who.userid'])
+        return user
+    else:
+        return None
+        
+def forceUser(user_name):
+    """ 
+    Force authentication 
+    @param user_name
+    """
+    request = tg.request
+    response = tg.response
+
+    request.cookies.clear()
+    authentication_plugins = request.environ['repoze.who.plugins']
+    identifier = authentication_plugins['main_identifier']
+
+    try:
+        response.headers = identifier.remember(request.environ, {'repoze.who.userid':user_name})
+    except:
+        pass
